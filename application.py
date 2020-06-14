fcd6dfe1dcf3e78ca93700dcbc16782690dcc6c2
Ofoedu Ebuka <frankofoedu@gmail.com>
2020-06-14 09:03:09 +0100

    new models

--- application.py
+++ application.py
@@ -1,12 +1,18 @@
 import io
 import json
-
+import torch
 from torchvision import models
 import torchvision.transforms as transforms
 from PIL import Image
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+device = torch.device('cpu')
+model = models.resnet18(num_classes=2)
+model.load_state_dict(torch.load('yorubamodel.pth',map_location=device))
+model.eval()
+
+
 
 @app.route('/')
 def hello():
@@ -28,8 +34,8 @@ def get_prediction(image_bytes):
     tensor = transform_image(image_bytes=image_bytes)
     outputs = model.forward(tensor)
     _, y_hat = outputs.max(1)
-    predicted_idx = str(y_hat.item())
-    return imagenet_class_index[predicted_idx]
+    return predicted_idx = str(y_hat.item())
+   # return imagenet_class_index[predicted_idx]
 
 
 @app.route('/predict', methods=['POST'])
@@ -37,8 +43,9 @@ def predict():
     if request.method == 'POST':
         file = request.files['file']
         img_bytes = file.read()
-        class_id, class_name = get_prediction(image_bytes=img_bytes)
-        return jsonify({'class_id': class_id, 'class_name': class_name})
+        #class_id, class_name = get_prediction(image_bytes=img_bytes)
+		class_name = get_prediction(image_bytes=img_bytes)
+        return jsonify({'class_id': 0, 'class_name': class_name})
 
     
 if __name__ == '__main__':
