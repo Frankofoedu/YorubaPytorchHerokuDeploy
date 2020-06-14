9d298abd6f0ae9f38fcbfb02cf5ebb052cefbce2
Ofoedu Frank Ebuka <Frankofoedu@gmail.com>
2020-06-14 11:14:33 +0100

    Update application.py

--- application.py
+++ application.py
@@ -19,34 +19,35 @@ def hello():
     return "Hello World!"
 
 
-def transform_image(image_bytes):
+def transform_image(infile):
     my_transforms = transforms.Compose([transforms.Resize(256),
                                         transforms.CenterCrop(224),
                                         transforms.ToTensor(),
                                         transforms.Normalize(
                                             [0.485, 0.456, 0.406],
                                             [0.229, 0.224, 0.225])])
-    image = Image.open(io.BytesIO(image_bytes))
-    return my_transforms(image).unsqueeze(0)
+    image = Image.open(infile)
+    timg = my_transforms(image)
+    timg.unsqueeze_(0)
+    return timg
 
 
-def get_prediction(image_bytes):
-    tensor = transform_image(image_bytes=image_bytes)
-    outputs = model.forward(tensor)
+def get_prediction(input_tensor):
+    outputs = model.forward(input_tensor)
     _, y_hat = outputs.max(1)
     predicted_idx = str(y_hat.item())
     return predicted_idx
-   # return imagenet_class_index[predicted_idx]
+    # return imagenet_class_index[predicted_idx]
 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
         file = request.files['file']
-        img_bytes = file.read()
-        #class_id, class_name = get_prediction(image_bytes=img_bytes)
-        class_name = get_prediction(image_bytes=img_bytes)
-        return jsonify({'class_id': 0, 'class_name': class_name})
+        if file is not None:
+            input_tensor = transform_image(file)
+            prediction_idx = get_prediction(input_tensor)            
+            return jsonify({'class_id': 0, 'class_name': prediction_idx})
 
     
 if __name__ == '__main__':
